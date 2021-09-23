a9a3155991058cac6ee42724d4daa577b2d84bcd
Anden Acitelli <a.acitelli@gmail.com>
2021-09-23 09:20:57 -0400

    Bugfix: Fix offset issue
    I assumed a coordinate was from the corner of the chunk but it was from the center :/

--- Assets/WorldGen/ChunkGen.cs
+++ Assets/WorldGen/ChunkGen.cs
@@ -16,6 +16,9 @@ public class TerrainType {
 // Generate chunks 
 public class ChunkGen : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject tilePrefab;
+
     // Edit different "biomes" from the editor 
     [SerializeField]
     private TerrainType[] terrainTypes;
@@ -49,28 +52,36 @@ public class ChunkGen : MonoBehaviour
     // Holds the Vector2 local coordinate of every vertex in the mesh.vertices array 
     void GenerateChunk()
     {
+        // Get chunk sizes
+        Vector3 tileSize = tilePrefab.GetComponent<MeshRenderer>().bounds.size;
+        int tileWidth = (int)tileSize.x;
+        int tileDepth = (int)tileSize.z;
+
         // Generate random list of vertices to use as our Delaunay vertices
         const int NUM_POINTS = 50;
         Polygon polygon = new Polygon();
-        Bounds bounds = this.meshFilter.mesh.bounds;
-        float minX = this.gameObject.transform.position.x, minZ = this.gameObject.transform.position.z;
+        float chunkX = this.gameObject.transform.position.x, chunkZ = this.gameObject.transform.position.z;
         for (int i = 0; i < NUM_POINTS; i++)
         {
             // Generate random vertices within chunk boundaries
             polygon.Add(new Vertex(
-                Random.Range(minX, minX + meshRenderer.bounds.size.x),
-                Random.Range(minZ, minZ + meshRenderer.bounds.size.z)));
+                Random.Range(chunkX - tileWidth / 2, chunkX + tileWidth / 2),
+                Random.Range(chunkZ - tileWidth / 2, chunkZ + tileWidth / 2)));
         }
 
         // Add the corners of the chunk
-        polygon.Add(new Vertex(minX, minZ));
-        polygon.Add(new Vertex(minX + meshRenderer.bounds.size.x, minZ));
-        polygon.Add(new Vertex(minX, minZ + meshRenderer.bounds.size.z));
-        polygon.Add(new Vertex(minX + meshRenderer.bounds.size.x, minZ + meshRenderer.bounds.size.z));
+        // TODO: Will introduce visual straight lines along chunk edges; use a hole-filling algorithm like the one detailed in this paper: https://kth.diva-portal.org/smash/get/diva2:1106042/FULLTEXT01.pdf
+        polygon.Add(new Vertex(chunkX - tileWidth / 2, chunkZ + tileDepth / 2));
+        polygon.Add(new Vertex(chunkX - tileWidth / 2, chunkZ - tileDepth / 2));
+        polygon.Add(new Vertex(chunkX + tileWidth / 2, chunkZ + tileDepth / 2));
+        polygon.Add(new Vertex(chunkX + tileWidth / 2, chunkZ - tileDepth / 2));
 
         // Let Triangle.NET do the hard work of actually generating the triangles to connect them
         TriangleNet.Meshing.ConstraintOptions options = new TriangleNet.Meshing.ConstraintOptions() { ConformingDelaunay = true } ;
         mesh = (TriangleNet.Mesh)polygon.Triangulate(options);
+        // GenerateMesh();
+        // this.meshFilter.mesh.vertices = mesh.vertices.Values
+        // this.meshFilter.mesh.triangles = mesh.triangles;
 
         // Convert from shared vertices to non-shared vertices; 
         // If we use shared vertices, there's just no way to get the low-poly shader style
@@ -79,10 +90,54 @@ public class ChunkGen : MonoBehaviour
         // Offset from overall world position based on coordinates
         // float offsetX = -this.gameObject.transform.position.x;
         // float offsetZ = -this.gameObject.transform.position.z;
+        // int chunkDepth = Mathf.RoundToInt(meshRenderer.bounds.size.z), chunkWidth = Mathf.RoundToInt(meshRenderer.bounds.size.x);
         // Dictionary<Vector2, float> heightMap = this.noise.GenerateNoiseMap(chunkDepth, chunkWidth, this.mapScale, offsetX, offsetZ, waves);
 
         // Update vertex heights and colors 
         // UpdateMeshVertices(heightMap);
+    }
+    public void GenerateMesh()
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector3> normals = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+        List<int> triangles = new List<int>();
+
+        IEnumerator<TriangleNet.Topology.Triangle> triangleEnum =
+        mesh.triangles.GetEnumerator();
+
+        for (int i = 0; i < mesh.Triangles.Count; i++)
+        {
+            if (!triangleEnum.MoveNext())
+            {
+                break;
+            }
+
+            TriangleNet.Topology.Triangle currentTriangle = triangleEnum.Current;
+
+            Vector3 v0 = new Vector3((float)currentTriangle.vertices[2].x, 0, (float)currentTriangle.vertices[2].y);
+            Vector3 v1 = new Vector3((float)currentTriangle.vertices[1].x, 0, (float)currentTriangle.vertices[1].y);
+            Vector3 v2 = new Vector3((float)currentTriangle.vertices[0].x, 0, (float)currentTriangle.vertices[0].y);
+
+            triangles.Add(vertices.Count);
+            triangles.Add(vertices.Count + 1);
+            triangles.Add(vertices.Count + 2);  
+
+            vertices.Add(v0);
+            vertices.Add(v1);
+            vertices.Add(v2);
+
+            var normal = Vector3.Cross(v1 - v0, v2 - v0);
+
+            for (int x = 0; x < 3; x++)
+            {
+                normals.Add(normal);
+                uvs.Add(Vector3.zero);
+            }
+        }
+
+        this.meshFilter.mesh.vertices = vertices.ToArray();
+        this.meshFilter.mesh.triangles = triangles.ToArray();
     }
 
     public void OnDrawGizmos()
