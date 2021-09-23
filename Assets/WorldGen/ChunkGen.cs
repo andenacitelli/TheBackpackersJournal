68dcd7e3ee24ea034a4be5995389e9fb71f14534
Anden Acitelli <a.acitelli@gmail.com>
2021-09-23 13:01:55 -0400

    Break/Progress. Chunks still spacing weirdly.

--- Assets/WorldGen/ChunkGen.cs
+++ Assets/WorldGen/ChunkGen.cs
@@ -18,6 +18,7 @@ public class ChunkGen : MonoBehaviour
 {
     [SerializeField]
     private GameObject tilePrefab;
+    private int tileWidth, tileDepth;
 
     // Edit different "biomes" from the editor 
     [SerializeField]
@@ -46,18 +47,20 @@ public class ChunkGen : MonoBehaviour
 
     void Start()
     {
+        // Calculate things we'll use all throughout the class 
+        Vector3 tileSize = tilePrefab.GetComponent<MeshRenderer>().bounds.size;
+        tileWidth = Mathf.RoundToInt(tileSize.x); // Should theoretically already be 10, but can't hurt to round to be sure
+        tileDepth = Mathf.RoundToInt(tileSize.z);
+
+        // Actually generate the chunk
         GenerateChunk(); 
     }
 
-    // Holds the Vector2 local coordinate of every vertex in the mesh.vertices array 
     void GenerateChunk()
     {
-        // Get chunk sizes
-        Vector3 tileSize = tilePrefab.GetComponent<MeshRenderer>().bounds.size;
-        int tileWidth = (int)tileSize.x;
-        int tileDepth = (int)tileSize.z;
-
         // Generate random list of vertices to use as our Delaunay vertices
+        // Seed this random generation off chunk coords so each chunk generates the same every time. May be off-by-one, but doesn't really matter.
+        Random.InitState(chunkSeedFromCoords(new Vector2Int((int) this.gameObject.transform.position.x / tileWidth, (int) this.gameObject.transform.position.x / tileDepth)));
         const int NUM_POINTS = 50;
         Polygon polygon = new Polygon();
         float chunkX = this.gameObject.transform.position.x, chunkZ = this.gameObject.transform.position.z;
@@ -66,7 +69,7 @@ public class ChunkGen : MonoBehaviour
             // Generate random vertices within chunk boundaries
             polygon.Add(new Vertex(
                 Random.Range(chunkX - tileWidth / 2, chunkX + tileWidth / 2),
-                Random.Range(chunkZ - tileWidth / 2, chunkZ + tileWidth / 2)));
+                Random.Range(chunkZ - tileDepth / 2, chunkZ + tileDepth / 2)));
         }
 
         // Add the corners of the chunk
@@ -79,23 +82,27 @@ public class ChunkGen : MonoBehaviour
         // Let Triangle.NET do the hard work of actually generating the triangles to connect them
         TriangleNet.Meshing.ConstraintOptions options = new TriangleNet.Meshing.ConstraintOptions() { ConformingDelaunay = true } ;
         mesh = (TriangleNet.Mesh)polygon.Triangulate(options);
-        // GenerateMesh();
-        // this.meshFilter.mesh.vertices = mesh.vertices.Values
-        // this.meshFilter.mesh.triangles = mesh.triangles;
 
-        // Convert from shared vertices to non-shared vertices; 
-        // If we use shared vertices, there's just no way to get the low-poly shader style
-        // UpdateTriangles();
+        // Actually convert Delaunay to a mesh and redo the triangles to give us flat shading
+        GenerateMesh();
 
-        // Offset from overall world position based on coordinates
-        // float offsetX = -this.gameObject.transform.position.x;
-        // float offsetZ = -this.gameObject.transform.position.z;
-        // int chunkDepth = Mathf.RoundToInt(meshRenderer.bounds.size.z), chunkWidth = Mathf.RoundToInt(meshRenderer.bounds.size.x);
-        // Dictionary<Vector2, float> heightMap = this.noise.GenerateNoiseMap(chunkDepth, chunkWidth, this.mapScale, offsetX, offsetZ, waves);
-
-        // Update vertex heights and colors 
-        // UpdateMeshVertices(heightMap);
+        // Update vertex heights and apply height-based coloration
+        List<Vector2> Vector3ToVector2(Vector3[] vList)
+        {
+            List<Vector2> output = new List<Vector2>();
+            foreach (Vector3 v in vList)
+            {
+                output.Add(new Vector2(v.x, v.z));
+            }
+            return output;
+        }
+        List<Vector2> vertices = Vector3ToVector2(this.meshFilter.mesh.vertices);
+        float offsetX = this.gameObject.transform.position.x;
+        float offsetZ = this.gameObject.transform.position.z;
+        List<float> noiseValues = this.noise.GenerateNoiseMap(vertices, this.mapScale, offsetX, offsetZ, waves);
+        UpdateVertexHeightsAndColors(noiseValues);
     }
+
     public void GenerateMesh()
     {
         List<Vector3> vertices = new List<Vector3>();
@@ -127,69 +134,31 @@ public class ChunkGen : MonoBehaviour
             vertices.Add(v1);
             vertices.Add(v2);
 
-            var normal = Vector3.Cross(v1 - v0, v2 - v0);
-
+            // var normal = Vector3.Cross(v1 - v0, v2 - v0);
+            
+            /* 
             for (int x = 0; x < 3; x++)
             {
-                normals.Add(normal);
+                // normals.Add(normal);
                 uvs.Add(Vector3.zero);
-            }
+            }'
+            */  
         }
 
         this.meshFilter.mesh.vertices = vertices.ToArray();
         this.meshFilter.mesh.triangles = triangles.ToArray();
     }
 
-    public void OnDrawGizmos()
+    // Low-collision function that gives us as unique an integer as possible, given chunk coordinates
+    int chunkSeedFromCoords(Vector2Int coords)
     {
-        if (mesh == null)
-        {
-            // We're probably in the editor
-            return;
-        }
+        // Parse will fail if the second number's ToString() puts a negative at the beginning, so we just 
+        // use the sign of the first one as the sign of whatever the overall method's output is
+        coords.y = coords.y < 0 ? coords.y * -1 : coords.y;
 
-        Gizmos.color = Color.red;
-        foreach (Edge edge in mesh.Edges)
-        {
-            Vertex v0 = mesh.vertices[edge.P0];
-            Vertex v1 = mesh.vertices[edge.P1];
-            Vector3 p0 = new Vector3((float)v0.x, 0.0f, (float)v0.y);
-            Vector3 p1 = new Vector3((float)v1.x, 0.0f, (float)v1.y);
-            Gizmos.DrawLine(p0, p1);
-        }
-    }
-
-    // TODO: This method is going to introduce straight lines along chunk boundaries, even if I keep those vertices the same
-    // Honestly not really sure what a good fix for that is without needing to pregenerate neighbor chunks so I can interpolate
-    // where the transition should be. It's a solution but seems complicated.
-    void RandomizeVertices()
-    { 
-        // Will technically give the same vertex-specific randomization for any chunks whose coordinates multiply
-        // to the same value, but collisions will be rare and those circumstances will be unrecognizeable
-        Random.InitState((int)this.gameObject.transform.position.x * (int)this.gameObject.transform.position.z);
-
-        int width = (int)Mathf.Sqrt(this.meshFilter.mesh.vertices.Length);
-        Vector3[] newVertices = new Vector3[this.meshFilter.mesh.vertices.Length];
-        int row, column;
-        for (int i = 0; i < this.meshFilter.mesh.vertices.Length; i++)
-        {
-            row = i / width;
-            column = i % width;
-            var vertex = this.meshFilter.mesh.vertices[i];
-
-            // If it's on the chunk boundary, we keep it the same so chunks are continuous across their borders
-            if (row == 0 || row == width - 1 || column == 0 || column == width - 1)
-            {
-                newVertices[i] = vertex;
-            } 
-            
-            // Otherwise, we can shake it up a little bit to give the game world some pizazz
-            else 
-            {
-                newVertices[i] = new Vector3(vertex.x + Random.Range(-.4f, .4f), vertex.y, vertex.z + Random.Range(-.4f, .4f));
-            }
-        }
-        this.meshFilter.mesh.vertices = newVertices;
+        // This is ugly, but it's infrequently called and is about as unique as you can get
+        // TODO: This will fail when you go very far due to chunk coord string concatenation parsing giving you something above the integer limit. *Highly* doubt this will come up later, so not worth finding a workaround right now.
+        return int.Parse(coords.x.ToString() + coords.y.ToString());
     }
 
     // Converts the vertex representation of the mesh to non-shared vertices 
@@ -202,12 +171,12 @@ public class ChunkGen : MonoBehaviour
             return new Vector3(v.x, v.y, v.z);
         }
 
-        // Three vertices for each end vertex
-        int width = (int) Mathf.Sqrt(this.meshFilter.mesh.vertices.Length);
+        Vector3 tileSize = tilePrefab.GetComponent<MeshRenderer>().bounds.size;
+        int width = (int)tileSize.x;
         int newVerticesPos = 0;
         Vector3[] newVertices = new Vector3[(int) Mathf.Pow(width, 2) * 6];
 
-        // We recalculate these 
+        // List of same order and length as normal vertex array that allows us to map a vertex index to a local chunk grid coordinate
         localVertexCoordinates = new Vector2[newVertices.Length];
 
         // Iterate through each vertex
@@ -274,34 +243,32 @@ public class ChunkGen : MonoBehaviour
     private AnimationCurve heightCurve;
 
     // Iterates through all vertices in a given Chunk and sets their heights based on their noise values 
-    private void UpdateMeshVertices(Dictionary<Vector2, float> heightMap)
+    // I'd normally split this into two functions for better cohesion, but color assignment uses the *perlin noise value*, not the height value.
+    //      We do it this way so we don't have to keep changing biome cutoffs if we change the map height scale constant.
+    private void UpdateVertexHeightsAndColors(List<float> heightmap)
     {
-        int tileDepth = (int) Mathf.Sqrt(heightMap.Count);
-        int tileWidth = tileDepth;
         Vector3[] meshVertices = this.meshFilter.mesh.vertices;
         Color[] colors = new Color[meshVertices.Length];
 
-        // Need a way to map the vertices to their subdivision values 
-        Bounds chunkBounds = this.meshFilter.mesh.bounds;
-        int vertexIndex = 0;
         for (int i = 0; i < meshVertices.Length; i++)
         {
-            // Update mesh height 
+            // This gets a little confusing to keep track of, so here are the things happening here:
+            // 1. The heightMap passed in is all [0, 1] Perlin map values.
+            // 2. We evaluate that value on our height curve (which lets us further control the
+            //      Perlin distribution on top of things like octaves and lacunarity that happen
+            //      during Perlin noise sampling) then multiply by a constant scaling factor that
+            //      makes it so this all doesn't range by like a meter.
             Vector3 vertex = meshVertices[i];
-            float height = Mathf.Clamp(heightMap[localVertexCoordinates[i]], .4f, 1);
-            // print("height: " + height);
-            meshVertices[vertexIndex] = new Vector3(vertex.x, this.heightCurve.Evaluate(height) * this.heightMultiplier, vertex.z);
-            // print("World height: " + this.heightCurve.Evaluate(height) * this.heightMultiplier);
-            //  print("Setting y-height to " + this.heightCurve.Evaluate(height) * this.heightMultiplier);
-            vertexIndex++;
+            float height = Mathf.Clamp(heightmap[i], 0, 1); // Clamp necessary because the heightCurve can't handle the *very* occasional points Perlin noise generates outside of [0, 1]
+            meshVertices[i] = new Vector3(vertex.x, this.heightCurve.Evaluate(height) * this.heightMultiplier, vertex.z);
 
             // Slightly randomize a given color so that a given height level doesn't look super monotone 
             Color tweakColor(float randomizationFactor, Color color)
             {
                 return new Color(
-                    color.r + Random.Range(-1 * randomizationFactor, randomizationFactor),
-                    color.g + Random.Range(-1 * randomizationFactor, randomizationFactor),
-                    color.b + Random.Range(-1 * randomizationFactor, randomizationFactor), 
+                    Mathf.Clamp(color.r + Random.Range(-1 * randomizationFactor, randomizationFactor), 0, 1),
+                    Mathf.Clamp(color.g + Random.Range(-1 * randomizationFactor, randomizationFactor), 0, 1),
+                    Mathf.Clamp(color.b + Random.Range(-1 * randomizationFactor, randomizationFactor), 0, 1), 
                     color.a);
             }
 
@@ -311,7 +278,7 @@ public class ChunkGen : MonoBehaviour
         }
 
         // Update actual mesh properties; basically "apply" the heights to the mesh 
-        this.meshFilter.mesh.colors = colors; // Apply color
+        this.meshFilter.mesh.colors = colors;
         this.meshFilter.mesh.vertices = meshVertices;
         this.meshFilter.mesh.RecalculateBounds();
         this.meshFilter.mesh.RecalculateNormals();
@@ -334,5 +301,25 @@ public class ChunkGen : MonoBehaviour
 
         // If we didn't hit one, choose the highest one 
         return terrainTypes[terrainTypes.Length - 1]; 
+    }
+
+    // Renders Triangulations on game pause
+    public void OnDrawGizmos()
+    {
+        if (mesh == null)
+        {
+            // We're probably in the editor
+            return;
+        }
+
+        Gizmos.color = Color.red;
+        foreach (Edge edge in mesh.Edges)
+        {
+            Vertex v0 = mesh.vertices[edge.P0];
+            Vertex v1 = mesh.vertices[edge.P1];
+            Vector3 p0 = new Vector3((float)v0.x, 0.0f, (float)v0.y);
+            Vector3 p1 = new Vector3((float)v1.x, 0.0f, (float)v1.y);
+            Gizmos.DrawLine(p0, p1);
+        }
     }
 }
